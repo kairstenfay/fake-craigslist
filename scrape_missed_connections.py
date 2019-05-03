a8d4707971cf6872e5a14c57336ec02f015206bb
KairstenFay <kairsten.fay@gmail.com>
2019-05-03 08:19:25 -0700

    Extract all posting links from mis page

--- scrape_missed_connections.py
+++ scrape_missed_connections.py
@@ -3,5 +3,8 @@ import requests
 
 url = 'https://seattle.craigslist.org/d/missed-connections/search/mis'
 html_doc = requests.get(url).text
-soup = BeautifulSoup(html_doc, 'html.parser')
-print(soup.find(id="sortable-results").find_all('li'))
+
+soup = BeautifulSoup(html_doc, 'html.parser').find(id="sortable-results")  # Jump straight to postings
+links_with_text = [a['href'] for a in soup.find_all('a', href=True) if a.text]  # Extract all links to postings
+links = list(filter(lambda x: x != '#', links_with_text))  # Remove '#' links
+print(links)
