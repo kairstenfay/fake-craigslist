4d914cd61ca471d471a6e206f8fcfc2fdc829aa1
KairstenFay <kairsten.fay@gmail.com>
2019-05-02 23:10:01 -0700

    wip

--- scrape_missed_connections.py
+++ scrape_missed_connections.py
@@ -2,5 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 
 url = 'https://seattle.craigslist.org/d/missed-connections/search/mis'
-html = requests.get(url).text
-print(html)
+html_doc = requests.get(url).text
+soup = BeautifulSoup(html_doc, 'html.parser')
+print(soup.find(id="sortable-results").find_all('li'))
