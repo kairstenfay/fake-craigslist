751f0266c12ab23c4245984ba5cb74328da274ae
KairstenFay <kairsten.fay@gmail.com>
2019-05-03 23:44:04 -0700

    checkpoint

--- scrape_missed_connections.py
+++ scrape_missed_connections.py
@@ -3,8 +3,19 @@ import requests
 
 url = 'https://seattle.craigslist.org/d/missed-connections/search/mis'
 html_doc = requests.get(url).text
+soup = BeautifulSoup(html_doc, 'html.parser').find(id="sortable-results")  # Jump to postings
 
-soup = BeautifulSoup(html_doc, 'html.parser').find(id="sortable-results")  # Jump straight to postings
-links_with_text = [a['href'] for a in soup.find_all('a', href=True) if a.text]  # Extract all links to postings
+links_with_text = [a['href'] for a in soup.find_all('a', href=True) if a.text]  # Extract post links
 links = list(filter(lambda x: x != '#', links_with_text))  # Remove '#' links
-print(links)
+
+store = []
+for link in links:
+    post_doc = requests.get(link).text
+    post = BeautifulSoup(post_doc, 'html.parser')
+
+    store.append({
+                "title": post.title,
+                "content": post.find(id="postingbody")
+    })
+
+print(store)
