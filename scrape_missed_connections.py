3ca025621a0ff79a9d0e753fef43f7e9e4a2ff41
Kairsten Fay <kairsten.fay@gmail.com>
2019-05-04 16:49:53 -0700

    wip

--- scrape_missed_connections.py
+++ scrape_missed_connections.py
@@ -1,28 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import html
 
-# url = 'https://seattle.craigslist.org/d/missed-connections/search/mis'
-# html_doc = requests.get(url).text
-# soup = BeautifulSoup(html_doc, 'html.parser').find(id="sortable-results")  # Jump to postings
+url = 'https://seattle.craigslist.org/d/missed-connections/search/mis'
+html_doc = requests.get(url).text
+soup = BeautifulSoup(html_doc, 'html.parser').find(id="sortable-results")  # Jump to postings
 
-# links_with_text = [a['href'] for a in soup.find_all('a', href=True) if a.text]  # Extract post links
-# links = list(filter(lambda x: x != '#', links_with_text))  # Remove '#' links
+links_with_text = [a['href'] for a in soup.find_all('a', href=True) if a.text]  # Extract post links
+links = list(filter(lambda x: x != '#', links_with_text))  # Remove '#' links
 
-link = 'https://seattle.craigslist.org/skc/mis/d/kent-2-the-female-at-renton-haul/6877187434.html'
 store = []
-#for link in links:
-if link:
+for link in links:
     post_doc = requests.get(link).text
     post = BeautifulSoup(post_doc, 'html.parser')
     content = post.find(id="postingbody")
-    subtree = content.p
-    subtree.extract()
+    content.p.extract()  # Remove <p> tag
+    stripped_content = content.text.strip()
 
     store.append({
                 "id": link[8:],  # Remove 'https://'
                 "title": str(post.title.string),
-                "content": str(content.text)
+                "content": stripped_content,
     })
     
     # var pID
@@ -35,5 +34,5 @@ if link:
     #     if i.text:
     #         print(str(counter) + ' ' + str(i.text) + '\n')
     #     counter+=1
-print(json.dumps(store))
+print(json.dumps(store, indent=4))
 
