fabe3a3a514cb9ffe2910dde24b835b125d9660a
Kairsten Fay <kairsten.fay@gmail.com>
2019-05-04 21:37:32 -0700

    work in progress
    wip
    
    wip
    
    Extract all posting links from mis page
    
    checkpoint
    
    checkpoint
    
    wip
    
    wip

---
+++ scrape_missed_connections.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import html
+
+url = 'https://seattle.craigslist.org/d/missed-connections/search/mis'
+html_doc = requests.get(url).text
+soup = BeautifulSoup(html_doc, 'html.parser').find(id="sortable-results")  # Jump to postings
+
+links_with_text = [a['href'] for a in soup.find_all('a', href=True) if a.text]  # Extract post links
+links = list(filter(lambda x: x != '#', links_with_text))  # Remove '#' links
+
+store = []
+
+for link in links:
+    post_doc = requests.get(link).text
+    post = BeautifulSoup(post_doc, 'html.parser')
+    content = post.find(id="postingbody")
+    content.p.extract()  # Remove <p> tag
+    stripped_content = content.text.strip()
+
+    store.append({
+        "id": link[8:len(link)-5],  # Remove 'https://' and '.html' from URL
+        "title": str(post.title.string),
+        "content": stripped_content
+    })
+    
+    # var pID
+    # if id="has_been_removed": 
+    # like with 'https://seattle.craigslist.org/see/mis/d/seattle-what-free-really-feels-like/6879541375.html'
+print(json.dumps(store, indent=4))
+
