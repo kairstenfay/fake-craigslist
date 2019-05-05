45bd688869c3723392171119cfe0c70a529b37cd
Wx <wangxin@immersionv.com>
2019-05-04 23:13:33 -0700

    Add city data to output. Refactor main.

--- scrape_missed_connections.py
+++ scrape_missed_connections.py
@@ -3,30 +3,65 @@ import requests
 import json
 import html
 
-url = 'https://seattle.craigslist.org/d/missed-connections/search/mis'
-html_doc = requests.get(url).text
-soup = BeautifulSoup(html_doc, 'html.parser').find(id="sortable-results")  # Jump to postings
 
-links_with_text = [a['href'] for a in soup.find_all('a', href=True) if a.text]  # Extract post links
-links = list(filter(lambda x: x != '#', links_with_text))  # Remove '#' links
+def main():
+    url = 'https://seattle.craigslist.org/d/missed-connections/search/mis'
+    store = []
 
-store = []
+    for link in get_post_list(url):
+        print(f"Attempting to retrieve data from {link}")
 
-for link in links:
-    post_doc = requests.get(link).text
-    post = BeautifulSoup(post_doc, 'html.parser')
+        post_doc = requests.get(link).text
+        post = BeautifulSoup(post_doc, 'html.parser')
+        store.append({
+            "id": link[8:len(link) - 5],  # Remove 'https://' and '.html' from URL
+            "geo": get_geo(post),
+            "title": post.title.string,
+            "content": get_content(post)
+        })
+
+        # var pID
+        # if id="has_been_removed":
+        # like with 'https://seattle.craigslist.org/see/mis/d/seattle-what-free-really-feels-like/6879541375.html'
+    print(json.dumps(store, indent=4))
+
+
+def get_post_list(url: str) -> list:
+    """
+    Given a URL, return a list of all URLs of the postings
+    """
+    html_doc = requests.get(url).text
+    soup = BeautifulSoup(html_doc, 'html.parser').find(id="sortable-results")  # Jump to postings
+    links_with_text = [a['href'] for a in soup.find_all('a', href=True) if a.text]  # Extract post links
+    links = list(filter(lambda x: x != '#', links_with_text))  # Remove '#' links
+    return links
+
+
+def get_content(post) -> str:
+    """
+    Given a BeautifulSoup object *post*, return the body of the message
+    """
     content = post.find(id="postingbody")
+    if not content:
+        print("Could not retrieve data from post")
+        return
     content.p.extract()  # Remove <p> tag
-    stripped_content = content.text.strip()
+    return content.text.strip()
 
-    store.append({
-        "id": link[8:len(link)-5],  # Remove 'https://' and '.html' from URL
-        "title": str(post.title.string),
-        "content": stripped_content
-    })
-    
-    # var pID
-    # if id="has_been_removed": 
-    # like with 'https://seattle.craigslist.org/see/mis/d/seattle-what-free-really-feels-like/6879541375.html'
-print(json.dumps(store, indent=4))
 
+def get_geo(post) -> str:
+    """
+    Given a BeautifulSoup object *post*, returns the affiliated city
+    """
+    geo = post.find_all("meta", {"name" : "geo.placename"})
+    if geo:
+        # grabs all the (one) meta tags where meta name=geo.placename, then takes the first item of the list
+        # THEN grabs the content out of the first item
+        geo_content = post.find_all("meta", {"name" : "geo.placename"})[0]['content']  # why does find not work
+    else:
+        geo_content = None
+    return geo_content
+
+
+if __name__ == "__main__":
+    main()
